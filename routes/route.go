1793f7492a2733962ed68db46017317c96a31533
Prajwal Pai <prajwalpai@Prajwals-MacBook-Pro.local>
2022-09-15 11:39:18 +0530

    remove unnecessary files

--- routes/route.go
+++ routes/route.go
@@ -3,16 +3,16 @@ package routes
 import(
 	"app/controllers"
 	"github.com/gin-gonic/gin"
-	//"net/http"
 	ginSwagger "github.com/swaggo/gin-swagger"
 	swaggerFiles "github.com/swaggo/files"
-	//"github.com/go-openapi/runtime/middleware"
 	"github.com/gin-contrib/cors"
 )
 
 func Profileroute(router *gin.Engine) {
-	//health check
+	
+	//CORS 
 	router.Use(cors.Default())
+	//health check
 	router.GET("api/v1/health",controllers.Healthcheck())
 	//insert
 	router.POST("api/v1/profile", controllers.InsertProfile())
@@ -20,9 +20,10 @@ func Profileroute(router *gin.Engine) {
 	router.GET("api/v1/profile/:id", controllers.GetProfile())
 	//update
 	router.PUT("api/v1/profile/:id", controllers.UpdateProfile())
+	//delete
 	router.DELETE("api/v1/profile/:id", controllers.DeleteProfile())
+	//serve swagger UI
 	router.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-	//router.Docs("/docs",http.FileServer(http.Dir("./")))
-	//router.GET("/users", controllers.GetAllUsers())
+
 
 }
