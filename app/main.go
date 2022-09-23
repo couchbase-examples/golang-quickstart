e89b119ba04288ddd62edbe4f3611cdf64eb2c35
Prajwal Pai <prajwalpai@Prajwals-MacBook-Pro.local>
2022-09-23 11:37:28 +0530

    Add Readme file

--- app/main.go
+++ app/main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
-
 	"app/routes"
 	"github.com/gin-gonic/gin"
-	_ "app/docs"
 	_ "app/controllers"
-
+	_ "app/docs"
+	//swaggerFiles "github.com/swaggo/files"
+	//ginSwagger "github.com/swaggo/gin-swagger"
 	_ "app/docs"
 )
 
+// @title Go Profile API
+// @version 1.0
+// @description Couchbase Golang Quickstart using Gin Gonic
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name MIT
+// @license.url https://opensource.org/licenses/MIT
+
+// @host localhost:8080
+// @BasePath /
+// @query.collection.format multi
 
 func main() {
 	router := gin.Default()
--- app/routes/route.go
+++ app/routes/route.go
@@ -1,19 +1,17 @@
 package routes
 
-import(
+import (
 	"app/controllers"
 	"github.com/gin-gonic/gin"
-	ginSwagger "github.com/swaggo/gin-swagger"
 	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 	"github.com/gin-contrib/cors"
 )
 
 func Profileroute(router *gin.Engine) {
-	
-	//CORS 
-	router.Use(cors.Default())
 	//health check
-	router.GET("api/v1/health",controllers.Healthcheck())
+	router.Use(cors.Default())
+	router.GET("api/v1/health", controllers.Healthcheck())
 	//insert
 	router.POST("api/v1/profile", controllers.InsertProfile())
 	//get
@@ -22,8 +20,9 @@ func Profileroute(router *gin.Engine) {
 	router.PUT("api/v1/profile/:id", controllers.UpdateProfile())
 	//delete
 	router.DELETE("api/v1/profile/:id", controllers.DeleteProfile())
-	//serve swagger UI
+	//swagger UI
 	router.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-
+	//search
+	router.GET("api/v1/profile/profiles", controllers.SearchProfile())
 
 }
